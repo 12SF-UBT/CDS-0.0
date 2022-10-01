6559946b17dbdc20fdba3f53f90bf50f7904e0ae
Surventurer <89982630+Surventurer@users.noreply.github.com>
2022-10-01 06:17:30 +0530

    Making code more beautiful

--- CDS-0.0.c
+++ CDS-0.0.c
@@ -1,8 +1,8 @@
 /* This is the main program that execute for further process. */
 # include <stdio.h> // Library
-void main(){    //Requesting from OS for this instruction to run.
-    int choice, i=1;    //Created two variable
-    while (i == 1) {    //This line insure that program never ends unless you type invalid input.
+void main(){    // Requesting from OS for this instruction to run.
+    int choice, i=1;    // Created two variable
+    while (i == 1) {    // This line insure that program never ends unless you type invalid input.
         printf(
           "\t   **************************************************\n"
           "           *             \tMain Menu             \t    *\n"
@@ -10,20 +10,20 @@ void main(){    //Requesting from OS for this instruction to run.
           "           *      \t1. Login\t            \t    *      \n"
           "           *      \t2. Create Account\t    \t    *\n"
           "           *      \t3. Exit\t              \t\t    *\n"
-          "\t   **************************************************\n\n");   //Basic UI for our program.
-        printf("Enter your choice: ");  //Asking choice for input.
-        scanf("%d", &choice);   //Storing input in variable choice.
+          "\t   **************************************************\n\n");   // Basic UI for our program.
+        printf("Enter your choice: ");  // Asking choice for input.
+        scanf("%d", &choice);   // Storing input in variable choice.
         switch (choice) {
-        case 1: //we have to create function.
+        case 1: // we have to create function.
             //login(); ---- Function name
             break;
-        case 2: //we have to create function.
+        case 2: // we have to create function.
             //addaccount(); ---- Function name
             break;
-        case 3: //It will exit the program
+        case 3: // It will exit the program
             i=0;
             break;
-        default:    //If someone enter (A-Z, a-z) or combination of (A-Z, a-z and 0-9) then shows invalid input and exit the program.
+        default:    // If someone enter (A-Z, a-z) or combination of (A-Z, a-z and 0-9) then shows invalid input and exit the program.
             printf("\n\tI N V A L I D  I N P U T  !\n");
             i=0;
         }
